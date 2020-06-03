1883db4ad0cbfe7708e8674854722726f8a0ca78
Alain Saint-Dic <alains214@gmail.com>
2020-06-03 20:07:01 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-
+  new_status = epic_tragedy[:hero[]]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
